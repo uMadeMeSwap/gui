fe0d9060fa77d3061b66104576b2dc0e77911438
ChefNakamoto <120574889+ChefNakamoto@users.noreply.github.com>
2023-02-06 08:34:56 -0500

    Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,10 +5,10 @@ export default function Dashboard() {
   return (
     <Container id="dashboard-page" className="py-4 mx-auto md:py-8 lg:py-12" maxWidth="2xl">
       <Head>
-        <title>Dashboard | Sushi</title>
-        <meta name="description" content="Sushi" />
-        <meta key="twitter:description" name="twitter:description" content="Sushi" />
-        <meta key="og:description" property="og:description" content="Sushi" />
+        <title>Dashboard | Anda</title>
+        <meta name="description" content="Anda" />
+        <meta key="twitter:description" name="twitter:description" content="Anda" />
+        <meta key="og:description" property="og:description" content="Anda" />
       </Head>
     </Container>
   )
